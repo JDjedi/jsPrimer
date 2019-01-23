62fa55853a2fee8da2acf8ec6aefb597b2d0a7aa
JDjedi <jcdiaz1201@gmail.com>
2019-01-22 23:03:55 -0600

    new to-do app made for practice

--- arrays/expenses.js
+++ arrays/expenses.js
@@ -1,5 +1,3 @@
-
-
 const account = {
   name: "Thor",
   ledger: [],
--- to-do-app/main.js
+++ to-do-app/main.js
@@ -1,15 +1,9 @@
 
-// Query and remove
-const img = document.querySelector('img')
-img.remove()
-
-
 // Query all and remove
 const ps = document.querySelectorAll('p')
 ps.forEach(function (p) {
 	//p.remove()
 	console.log(p.textContent) // shows the selected element its contents
-	p.textContent = '******' // changes selected elemtent and changes its contents
+	p.textContent = 'completed!' // changes selected elemtent and changes its contents
 })
-
 
