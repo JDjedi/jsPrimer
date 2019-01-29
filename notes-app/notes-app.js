2f778b9d21a9511a7c5913ad88d77fdd19e99fc7
JDjedi <jcdiaz1201@gmail.com>
2019-01-29 12:59:11 -0600

    added new functionality to note app and todo app

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -3,13 +3,16 @@
 const img = document.querySelector('img')
 img.remove()
 
-
 // Query all and remove
-const ps = document.querySelectorAll('p')
-ps.forEach(function (p) {
-	//p.remove()
-	console.log(p.textContent) // shows the selected element its contents
-	p.textContent = '******' // changes selected elemtent and changes its contents
+// const ps = document.querySelectorAll('p')
+// ps.forEach(function (p) {
+// 	//p.remove()
+// 	console.log(p.textContent) // shows the selected element its contents
+// 	p.textContent = '******' // changes selected elemtent and changes its contents
+// })
+
+// adds an event listener to button, first argument is what you are listenting for, the second what you want it to do
+// target.addEventListener(type, listener)
+document.querySelector('button').addEventListener('click', function(event) { // event arg is useful when you want to do something with the element it represents, in this case the button tag
+	event.target.textContent = 'Note Created!'
 })
-
-
--- to-do-app/main.js
+++ to-do-app/main.js
@@ -21,44 +21,55 @@ const toDos = [
   }
 ];
 
+document
+  .querySelector("#remove-all-button")
+  .addEventListener("click", function() {
+    document.querySelectorAll("p").forEach(function(note) {
+      note.remove();
+    });
+  });
+
+document
+  .querySelector("#sort-todos-button")
+  .addEventListener("click", function() {
+    toDos.sort(function(a, b) {
+      //    return -1 // returns a first!
+      //    return 1 // returns b first!
+      //    return 0 // order does not need to be changes
+      if (a.completion < b.completion) {
+        return -1;
+      } else if (b.completion < a.completion) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    const newParagraph = document.querySelector("p"); // querySelector() used for id's primarily
+    newParagraph.textContent =
+      "List sorted! Also, I LOVE MARIELLE SHE IS MY BOO!";
+  });
+
+document
+  .querySelector("#show-todos-button")
+  .addEventListener("click", function() {
+    toDos.forEach(function(post) {
+      // dont need to pass in the list as an argument bc the scoping rules allow you to grab the list from the global scope
+      if (post.completion == false) {
+        let newParagraph = document.createElement("p");
+        newParagraph.textContent = post.task;
+        document.querySelector("body").appendChild(newParagraph);
+      }
+    });
+  });
+
+// console.log(toDos)
+//
 // old functionality
 // const ps = document.querySelectorAll('p')
 
 // ps.forEach(function (p) {
-// 	//p.remove()
-// 	console.log(p.textContent) // shows the selected element its contents
-// 	p.textContent = 'completed!' // changes selected elemtent and changes its contents
+//  //p.remove()
+//  console.log(p.textContent) // shows the selected element its contents
+//  p.textContent = 'completed!' // changes selected elemtent and changes its contents
 // })
-
-
-const sortToDos = function(toDosList) {
-  toDosList.sort(function(a, b) {
-    //    return -1 // returns a first!
-    //    return 1 // returns b first!
-    //    return 0 // order does not need to be changes
-    if (a.completion < b.completion) {
-      return -1;
-    } else if (b.completion < a.completion) {
-      return 1;
-    } else {
-      return 0;
-    }
-  });
-};
-
-sortToDos(toDos);
-
-toDos.forEach(function (post) {
-  if (post.completion == false) {
-    let newParagraph = document.createElement('p')
-    newParagraph.textContent = post.task
-    document.querySelector('body').appendChild(newParagraph)
-  }
-
-})
-// console.log(toDos)
-
-
-
-
-
