b235b2ab6efb62ce7fb8cddd783e4913b6535736
JDjedi <jcdiaz1201@gmail.com>
2019-03-04 10:41:16 -0600

    Finished class conversion

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,48 +1,47 @@
 
-const Hangman = function(wordToGuess, numberOfGuesses) {
+
+class HangmanClass {
+	constructor(wordToGuess, numberOfGuesses, guessedLetters = []) {
 	this.wordToGuess = wordToGuess.toLowerCase().split("")
 	this.numberOfGuesses = numberOfGuesses
-	let gameGuess = null
+	this.guessedLetters = guessedLetters
+	}
+
+	startGame() {
+		let answerPiece = ''
+		let guessedLetters = this.guessedLetters
+
+		this.wordToGuess.forEach(function(e) {
+			if (guessedLetters.includes(e)) {
+				answerPiece += e
+			} else {
+				answerPiece += '*'
+			}
+		})
+		return answerPiece
+	}
+
+	takeAGuess(userGuess) {
+		let guesses = []
+		let guessCount = this.numberOfGuesses
+
+		userGuess.forEach(function(e) {
+			e.toLowerCase()
+			if (!(guesses.includes(e)) && (guesses.length < guessCount)) {
+				guesses.push(e);
+			} else if(guesses.includes(e)) {
+				console.log(`You already guessed the letter ${e}`)
+			} else if(guesses.length >= guessCount) {
+				console.log(`You have made too many guesses, the letter ${e} will not be included`)
+			} else {
+				console.log("Something else went wrong LOL")
+			}
+		})
+		this.guessedLetters = guesses
+	}
 }
 
-Hangman.prototype.startGame = function(userGues) {
-	let answerPiece = ''
-	this.wordToGuess.forEach(function(e) {
-		if (userGuess.includes(e)) {
-			answerPiece += userGuess
-		} else {
-			answerPiece += '*'
-		}
-	})
-	return answerPiece
-}
 
-Hangman.prototype.takeAGuess = function(userGuess) {
-	let guesses = []
-	let guessCount = this.numberOfGuesses
-
-	userGuess.forEach(function(e) {
-		e.toLowerCase()
-		if (!(guesses.includes(e)) && (guesses.length < guessCount)) {
-			guesses.push(e);
-		} else if(guesses.includes(e)) {
-			console.log(`You already guessed the letter ${e}`)
-		} else if(guesses.length >= guessCount) {
-			console.log(`You have made too many guesses, the letter ${e} will not be included`)
-		} else {
-			console.log("Something else went wrong LOL")
-		}
-	})
-	this.gameGuess = guesses
-	return this.gameGuess
-}
-
-
-const game = new Hangman("Alexander", 3);
-game.takeAGuess(["q", "L", "t"])
-game.startGame()
-
-// window.addEventListener('keypress', function(e) {
-// 	let guess = String.fromCharCode(e.charCode);
-// 	game.takeAGuess(guess);
-// })
+const game = new HangmanClass("Alexander", 10);
+game.takeAGuess(["a", "l", "e", "p", "d"])
+console.log(game.startGame())
